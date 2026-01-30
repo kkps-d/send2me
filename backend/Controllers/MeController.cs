cd6da46d511b6195a75cc499ec5561804626be8c
kkps-d <freakykaung@gmail.com>
2026-01-30 16:42:33 +0630

    add a me controller to return currently logged in user info

---
+++ backend/Controllers/MeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace backend.Controllers
+{
+    [Route("api/v1/me")]
+    [ApiController]
+    public class MeController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet("", Name = "GetMe")]
+        public async Task<IActionResult> Me()
+        {
+            string? username = Request.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (string.IsNullOrEmpty(username))
+            {
+                return StatusCode(500);
+            }
+
+            // TODO: In the future, could send down preferences too, move this to a service
+            return Ok(new { username });
+        }
+    }
+}
