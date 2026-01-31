ff9606dc165e06872ccf9ae08abb6b40490e118f
kkps-d <freakykaung@gmail.com>
2026-01-31 18:38:29 +0630

    update checkbox to use a hidden input:checkbox to support tab index and space to click

--- frontend/src/components/Checkbox/Checkbox.tsx
+++ frontend/src/components/Checkbox/Checkbox.tsx
@@ -5,25 +5,31 @@ type CheckboxProps = {
   checked?: boolean;
   setChecked?: React.Dispatch<React.SetStateAction<boolean>>;
   label?: string;
+  tabIndex?: number;
 };
 
 export function Checkbox({
   checked,
   setChecked,
   label = "Checkbox",
+  tabIndex = 0,
 }: CheckboxProps) {
   const [_checked, _setChecked] = useState(false);
-
   const checkedState = checked !== undefined ? checked : _checked;
-
   const setCheckedState = setChecked !== undefined ? setChecked : _setChecked;
-
-  const toggledChecked = () => {
+  const toggleChecked = () => {
     setCheckedState((state) => !state);
   };
 
   return (
-    <div className={styles.container} onClick={toggledChecked}>
+    <div className={styles.container}>
+      <input
+        type="checkbox"
+        className={styles.hiddenCheckbox}
+        tabIndex={tabIndex}
+        checked={checked}
+        onClick={() => toggleChecked()}
+      />
       <div className={`${styles.checkbox}`}>
         {checkedState && (
           <span className="material-symbols-outlined">check</span>
