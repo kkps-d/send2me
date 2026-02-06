c3ac0ce85dedaa652d496d3b2d097a4180240660
kkps-d <freakykaung@gmail.com>
2026-02-06 16:55:01 +0630

    Add a context menu to the user button with logout options

--- frontend/src/components/Nav/Nav.tsx
+++ frontend/src/components/Nav/Nav.tsx
@@ -1,13 +1,17 @@
 import styles from "./Nav.module.css";
 import { FlexSpacer } from "../FlexSpacer/FlexSpacer";
 import { getPreferredColorScheme } from "../../utils/getPreferredColorScheme";
-import { useState } from "react";
+import { useRef, useState, type RefObject } from "react";
 import { Logo } from "../Logo/Logo";
 import { Button } from "../Button/Button";
 import { useAuth } from "../../contexts/AuthContext";
+import { Overlay } from "../Overlay/Overlay";
+import { ContextMenu, ContextMenuItem } from "../Overlay/Modals/ContextMenu";
 
 export function Nav() {
   const { user, isLoading } = useAuth();
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const [overlay, setOverlay] = useState<React.ReactNode>(null);
 
   const [colorTheme, setColorTheme] = useState<"light" | "dark">(
     getPreferredColorScheme(),
@@ -21,6 +25,15 @@ export function Nav() {
       document.documentElement.classList.add(theme);
       return theme;
     });
+  }
+
+  function onClickUserBtn() {
+    setOverlay(
+      <UserContextMenu
+        buttonRef={buttonRef}
+        onClose={() => setOverlay(null)}
+      />,
+    );
   }
 
   return (
@@ -49,7 +62,34 @@ export function Nav() {
         loading={isLoading}
         icon={<span className="material-symbols-outlined">person</span>}
         label={user?.username || "..."}
+        ref={buttonRef}
+        onClick={onClickUserBtn}
       />
+      {overlay}
     </div>
+  );
+}
+
+type UserContextMenuProps = {
+  buttonRef: RefObject<HTMLButtonElement | null>;
+  onClose?: () => void;
+};
+
+function UserContextMenu({ buttonRef, onClose }: UserContextMenuProps) {
+  return (
+    <Overlay onClose={onClose}>
+      <ContextMenu anchorRef={buttonRef} anchorPosition="right">
+        <ContextMenuItem
+          label="Settings"
+          icon={<span className="material-symbols-outlined">settings</span>}
+          onClick={() => console.log("ZAZA")}
+        />
+        <ContextMenuItem
+          label="Sign out"
+          color="#FF3B30"
+          icon={<span className="material-symbols-outlined">logout</span>}
+        />
+      </ContextMenu>
+    </Overlay>
   );
 }
