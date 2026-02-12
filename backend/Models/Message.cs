e04aa99eabb934de601b27bb6f551a9d80d5f2a7
kkps-d <freakykaung@gmail.com>
2026-02-12 18:36:40 +0630

    add empty defaults to Message model

--- backend/Models/Message.cs
+++ backend/Models/Message.cs
@@ -5,9 +5,8 @@ namespace backend.Models
     public class Message
     {
         public int MessageId { get; set; }
-        public string UserId { get; set; }
-
+        public string UserId { get; set; } = string.Empty;
         public DateTimeOffset CreatedAt { get; set; }
-        public string Content { get; set; }
+        public string Content { get; set; } = string.Empty;
     }
 }
