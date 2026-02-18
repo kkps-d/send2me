6be9765c83f8f6cdded24593480bb2aa4359db46
kkps-d <freakykaung@gmail.com>
2026-02-18 16:13:45 +0630

    add createMessage mutation

--- frontend/src/queries/useMessages.ts
+++ frontend/src/queries/useMessages.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Route } from "../routes/__root";
 import { BASE_URL } from "../utils/baseUrl";
 import z from "zod";
@@ -13,11 +13,19 @@ const messageResponseSchema = z.object({
   messages: z.array(messageDtoSchema),
   nextCursor: z.string().optional(),
 });
+const newMessageSuccessResponseSchema = z.object({
+  newMessageId: z.number(),
+});
 export type MessageDto = z.infer<typeof messageDtoSchema>;
+type NewMessageDto = {
+  content: string;
+};
 //#endregion
 
+// TODO: Probably should put mutate messages into a separate hook, since the msgactionbar doesn't really care about reading messages. Should reduce unnecessary refreshes?
 export function useMessages(cursor?: string) {
   const { auth } = Route.useRouteContext();
+  const queryClient = useQueryClient();
 
   async function fetchMessages() {
     const url = `${BASE_URL}/api/v1/messages`;
@@ -35,23 +43,53 @@ export function useMessages(cursor?: string) {
     return data;
   }
 
-  const { isSuccess, isLoading, error, data } = useQuery({
+  async function createMessage(content: string) {
+    // TODO: Error handling
+    const dto: NewMessageDto = {
+      content,
+    };
+    const url = `${BASE_URL}/api/v1/messages`;
+    const result = await auth.aFetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(dto),
+    });
+    const { success, error, data } = newMessageSuccessResponseSchema.safeParse(
+      await result.json(),
+    );
+
+    if (!success) {
+      throw new Error(JSON.stringify(error, null, 2));
+    }
+
+    return data;
+  }
+
+  const query = useQuery({
     queryKey: ["messages", cursor],
     queryFn: fetchMessages,
     retry: false,
   });
 
-  if (error) {
-    const zodError = JSON.parse(error.message);
+  const createMessageMutation = useMutation({
+    mutationFn: createMessage,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["messages"] }),
+  });
+
+  if (query.error) {
+    const zodError = JSON.parse(query.error.message);
     throw new Error(
       `API response does not match expected schema!\n${zodError.name}\n${zodError.message}`,
     );
   }
 
   return {
-    isSuccess,
-    isLoading,
-    messages: data?.messages,
-    nextCursor: data?.nextCursor,
+    queryLoading: query.isLoading,
+    querySucess: query.isSuccess,
+    messages: query.data?.messages,
+    nextCursor: query.data?.nextCursor,
+    createMessage: createMessageMutation.mutateAsync,
+    createPending: createMessageMutation.isPending,
+    createSuccess: createMessageMutation.isSuccess,
   };
 }
