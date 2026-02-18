8cf61cdb8d01dca01ee28fdffd7b399598fb6540
kkps-d <freakykaung@gmail.com>
2026-02-18 16:14:03 +0630

    update bottom bar to use textarea and add send function

--- frontend/src/components/MsgActionBar/MsgActionBar.tsx
+++ frontend/src/components/MsgActionBar/MsgActionBar.tsx
@@ -1,14 +1,37 @@
+import { useRef, useState } from "react";
 import { Button } from "../Button/Button";
 import { FlexSpacer } from "../FlexSpacer/FlexSpacer";
 import styles from "./MsgActionBar.module.css";
+import { useMessages } from "../../queries/useMessages";
 
 export function MsgActionBar() {
+  const [text, setText] = useState("");
+
+  const { createMessage } = useMessages();
+
+  function onSend() {
+    // TODO: Need to figure out the scroll to bottom logic
+    createMessage(text);
+    onClear();
+  }
+
+  function onClear() {
+    setText("");
+  }
+
   return (
     <div className={`${styles.container}`}>
-      <input
-        className="baseinput"
-        type="text"
-        placeholder="Type something, or drop files here"
+      <textarea
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" && e.shiftKey) {
+            e.preventDefault();
+            onSend();
+          }
+        }}
+        className="basetextarea"
+        placeholder="Type something, or drop files here. Shift + Enter to send."
       />
       <div className={styles.buttonGroup}>
         <Button
@@ -19,11 +42,13 @@ export function MsgActionBar() {
         <Button
           label="Clear"
           icon={<span className="material-symbols-outlined">close</span>}
+          onClick={onClear}
         />
         <Button
           label="Send"
           iconPosition="after"
           icon={<span className="material-symbols-outlined">send</span>}
+          onClick={onSend}
         />
       </div>
     </div>
